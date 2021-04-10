3de894794a3da0419938d7293e258ccaba4aa75a
Vinicius <vinicius.assis@acct.global>
2021-04-09 21:53:53 -0300

    feat: create Flex component

---
+++ src/components/Flex/index.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+type FlexProps = {
+  column?: boolean
+  justify?: string
+  align?: string
+  margin?: string
+  mb?: string
+  mt?: string
+  mr?: string
+  ml?: string
+}
+
+const Flex = styled.div<FlexProps>`
+  display: flex;
+  width: 100%;
+  ${({ column }) => (column ? 'flex-direction: column;' : '')}
+  ${({ justify }) => (justify ? `justify-content: ${justify};` : '')}
+  ${({ align }) => (align ? `align-items: ${align};` : '')}
+  ${({ margin }) => (margin ? `margin: ${margin};` : '')}
+  ${({ mb }) => (mb ? `margin-bottom: ${mb};` : '')}
+  ${({ mt }) => (mt ? `margin-top: ${mt};` : '')}
+  ${({ mr }) => (mr ? `margin-right: ${mr};` : '')}
+  ${({ ml }) => (ml ? `margin-left: ${ml};` : '')}
+`
+
+export default Flex
