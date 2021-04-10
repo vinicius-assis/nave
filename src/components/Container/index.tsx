783f79f12b76796ed41e130f4a3d104986b7845f
Vinicius <vinicius.assis@acct.global>
2021-04-09 22:20:10 -0300

    feat: create Container components

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100vw;
+  max-width: 1280px;
+  padding: 0 32px;
+  display: flex;
+  margin: 0 auto;
+`
+
+export default Container
