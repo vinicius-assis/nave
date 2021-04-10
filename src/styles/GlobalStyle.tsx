44bcd3b9be7c9ca5645587f169e7232a96c270ef
Vinicius <vinicius.assis@acct.global>
2021-04-09 21:53:05 -0300

    feat(style): create globalStyle

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
+  
+  * {
+    color: #212121;
+    font-family: "Montserrat", sans-serif;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    outline: none;
+  }
+`
+
+export default GlobalStyle
