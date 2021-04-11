6457d9f4dc8e7277c451b11cc27fbf6e56867b1e
Vinicius <vinicius.assis@acct.global>
2021-04-10 22:57:46 -0300

    feat: add props to set margin in Titles

--- src/components/Titles/index.tsx
+++ src/components/Titles/index.tsx
@@ -2,6 +2,9 @@ import styled from 'styled-components'
 
 type TitleProps = {
   ml?: string
+  mr?: string
+  mt?: string
+  mb?: string
 }
 
 export const Title = styled.h1<TitleProps>`
@@ -9,15 +12,26 @@ export const Title = styled.h1<TitleProps>`
   line-height: 36px;
   font-weight: bold;
   ${({ ml }) => (ml ? `margin-left: ${ml};` : '')}
+  ${({ mr }) => (mr ? `margin-right: ${mr};` : '')}
+  ${({ mt }) => (mt ? `margin-top: ${mt};` : '')}
+  ${({ mb }) => (mb ? `margin-bottom: ${mb};` : '')}
 `
-export const ProfileTitle = styled.h3`
+export const ProfileTitle = styled.h3<TitleProps>`
   font-size: 16px;
   font-weight: bold;
   margin-bottom: 4px;
+  ${({ ml }) => (ml ? `margin-left: ${ml};` : '')}
+  ${({ mr }) => (mr ? `margin-right: ${mr};` : '')}
+  ${({ mt }) => (mt ? `margin-top: ${mt};` : '')}
+  ${({ mb }) => (mb ? `margin-bottom: ${mb};` : '')}
 `
 
-export const ProfileSubTitle = styled.h5`
+export const ProfileSubTitle = styled.h5<TitleProps>`
   font-size: 16px;
   font-weight: normal;
   margin-bottom: 14px;
+  ${({ ml }) => (ml ? `margin-left: ${ml};` : '')}
+  ${({ mr }) => (mr ? `margin-right: ${mr};` : '')}
+  ${({ mt }) => (mt ? `margin-top: ${mt};` : '')}
+  ${({ mb }) => (mb ? `margin-bottom: ${mb};` : '')}
 `
